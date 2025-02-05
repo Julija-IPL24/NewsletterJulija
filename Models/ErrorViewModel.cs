60cdb58143317d65de8703c9f5494e15a45cc6cb
89julgel <89julgel@gafe.molndal.se>
2025-02-05 10:31:05 +0100

    MVC added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NewsletterJulija.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
